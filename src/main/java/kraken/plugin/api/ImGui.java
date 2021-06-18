04abe58e1ccfd1754b289fc509975fa51bb41358
anonymous <a@google.com>
2021-06-18 02:28:59 -0700

    wups

--- src/main/java/kraken/plugin/api/ImGui.java
+++ src/main/java/kraken/plugin/api/ImGui.java
@@ -13,7 +13,7 @@ public class ImGui {
     public static native boolean checkbox(String text, boolean initial);
     public static native int intSlider(String text, int v, int min, int max);
     public static native boolean button(String text);
-    public static native String input(String text, byte[] input);
+    public static native void input(String text, byte[] input);
     public static native int intInput(String text, int v);
 
 }
