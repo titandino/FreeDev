9ad79e09c37dce6e769d6cd8e53851c1c2aea8f4
Trent <trenton.kress@gmail.com>
2021-07-13 10:40:30 -0700

    Generalize hall of memories script and improve logic flow and action prioritization

--- src/main/java/com/darkan/scripts/hom/HallOfMemories.java
+++ src/main/java/com/darkan/scripts/hom/HallOfMemories.java
@@ -14,76 +14,76 @@ import kraken.plugin.api.Time;
 
 @Script("Hall of Memories")
 public class HallOfMemories extends ScriptSkeleton {
-  
-    private static final int MEMORY_JAR_EMPTY = 42898;
-    private static final int MEMORY_JAR_PARTIAL = 42899;
-    private static final int MEMORY_JAR_FULL = 42900;
-    private static final int CORE_MEMORY_FRAGMENT_ITEM = 42901;
-	
+
+	private static final int MEMORY_JAR_EMPTY = 42898;
+	private static final int MEMORY_JAR_PARTIAL = 42899;
+	private static final int MEMORY_JAR_FULL = 42900;
+	private static final int CORE_MEMORY_FRAGMENT_ITEM = 42901;
+
 	private int startXp;
-    private NPC memory;
-	
+	private NPC memory;
+
 	public HallOfMemories() {
 		super("Hall Of Memories", 1200);
 	}
-	
+
 	@Override
 	public boolean onStart(Player self) {
-        setState("Starting...");
-        startXp = Client.getStatById(Client.DIVINATION).getXp();
-        start();
+		setState("Starting...");
+		startXp = Client.getStatById(Client.DIVINATION).getXp();
+		start();
 		return true;
 	}
-	
+
 	@Override
 	public void loop(Player self) {
-        if (self.isMoving())
-            return;
-		if (Interfaces.getInventory().freeSlots() > 5) {
-		    if (WorldObjects.interactClosest("Take-from", object -> object.getName().equals("Jar depot"))) {
-		        setState("Gathering empty jars.");
-                sleepWhile(2400, 25000, () -> Interfaces.getInventory().freeSlots() > 5 || self.isMoving());
-		    }
-        }
-        if ((Interfaces.getInventory().contains(MEMORY_JAR_EMPTY, 1) || Interfaces.getInventory().contains(MEMORY_JAR_PARTIAL, 1)) && Interfaces.getInventory().isFull()) {
-            if (!Interfaces.getInventory().isFull() || Interfaces.getInventory().contains(CORE_MEMORY_FRAGMENT_ITEM, 1)) {
-                if (NPCs.interactClosestReachable("Capture", npc -> npc.getName().equals("Core memory fragment"))) {
-                    setState("Getting core memory fragment.");
-                    sleepWhile(3000, 12000, () -> self.isAnimationPlaying() || self.isMoving());
-                    return;
-                }
-            }
-            if (NPCs.interactClosestReachable("Capture", npc -> npc.getName().equals("Knowledge fragment"))) {
-                setState("Getting knowledge fragments.");
-                return;
-            }
-            if (!self.isAnimationPlaying())
-                memory = HOMConfig.getNPCForLevel(Client.getStatById(Client.DIVINATION).getCurrent());
-            if (memory != null)
-                memory.interact("Harvest");
-            setState("Filling memory jars.");
-        } else if (Interfaces.getInventory().contains(MEMORY_JAR_FULL, 1)) {
-            if (!self.isAnimationPlaying()) {
-                if (WorldObjects.interactClosest("Offer-memory")) {
-	                setState("Depositing memory jars.");
-	                sleepWhile(5000, 75000, () -> self.isAnimationPlaying() || self.isMoving());
-                }
-            }
-        }
-    }
+		if (self.isMoving())
+			return;
+		
+		if ((!Interfaces.getInventory().isFull() || Interfaces.getInventory().contains(CORE_MEMORY_FRAGMENT_ITEM, 1)) 
+				&& NPCs.interactClosestReachable("Capture", npc -> npc.getName().equals("Core memory fragment"))) {
+			setState("Getting core memory fragment.");
+			sleepWhile(3000, 12000, () -> self.isAnimationPlaying() || self.isMoving());
+			return;
+		}
+		if (NPCs.interactClosestReachable("Capture", npc -> npc.getName().equals("Knowledge fragment"))) {
+			setState("Getting knowledge fragments.");
+			sleepWhile(3000, 12000, () -> self.isAnimationPlaying() || self.isMoving());
+			return;
+		}
+		if (Interfaces.getInventory().freeSlots() > 5 && WorldObjects.interactClosest("Take-from", object -> object.getName().equals("Jar depot"))) {
+			setState("Gathering empty jars.");
+			sleepWhile(2400, 25000, () -> Interfaces.getInventory().freeSlots() > 5 || self.isMoving());
+			return;
+		}
+		if ((Interfaces.getInventory().contains(MEMORY_JAR_EMPTY, 1) || Interfaces.getInventory().contains(MEMORY_JAR_PARTIAL, 1))) {
+			if (!self.isAnimationPlaying())
+				memory = HOMConfig.getNPCForLevel(Client.getStatById(Client.DIVINATION).getCurrent());
+			if (memory != null)
+				memory.interact("Harvest");
+			setState("Filling memory jars.");
+		} else if (Interfaces.getInventory().contains(MEMORY_JAR_FULL, 1)) {
+			if (!self.isAnimationPlaying()) {
+				if (WorldObjects.interactClosest("Offer-memory")) {
+					setState("Depositing memory jars.");
+					sleepWhile(5000, 75000, () -> self.isAnimationPlaying() || self.isMoving());
+				}
+			}
+		}
+	}
 
-    @Override
-    public void paintImGui(long runtime) {
-        ImGui.label("XP p/h: " + Time.perHour(runtime, Client.getStatById(Client.DIVINATION).getXp() - startXp));
-    }
+	@Override
+	public void paintImGui(long runtime) {
+		ImGui.label("XP p/h: " + Time.perHour(runtime, Client.getStatById(Client.DIVINATION).getXp() - startXp));
+	}
 
-    @Override
-    public void paintOverlay(long runtime) {
-        
-    }
+	@Override
+	public void paintOverlay(long runtime) {
 
-    @Override
-    public void onStop() {
-        
-    }
+	}
+
+	@Override
+	public void onStop() {
+
+	}
 }
