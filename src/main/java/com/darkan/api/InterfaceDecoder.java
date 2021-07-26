3bf5c9cb5ccfb18420a843c1de9a3d00ad3a03e0
Trent <trenton.kress@gmail.com>
2021-07-26 08:14:04 -0700

    Add new item list and npc list dumps

--- src/main/java/com/darkan/api/InterfaceDecoder.java
+++ src/main/java/com/darkan/api/InterfaceDecoder.java
@@ -1,8 +1,10 @@
 package com.darkan.api;
 
+import java.io.IOException;
+
 public class InterfaceDecoder {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int hash = 93716496;
 		int interfaceId = hash >> 16;
 		int slotId = (hash & 0xFF);
